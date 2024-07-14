21fc2eef3a8590da998847dce585fe2b5c71a2ae
ivgarciag93 <ivgarciag93@gmail.com>
2024-07-14 18:23:35 +0200

    Change header and minor changes

--- app/src/main/java/com/ivione93/mytravel2/screens/SplashScreen.kt
+++ app/src/main/java/com/ivione93/mytravel2/screens/SplashScreen.kt
@@ -1,6 +1,5 @@
 package com.ivione93.mytravel2.screens
 
-import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -17,11 +16,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import com.ivione93.mytravel2.R
 import com.ivione93.mytravel2.ui.theme.MyTypography
@@ -29,14 +26,11 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    // Estamos simulando la espera de 5 seg
     LaunchedEffect(key1 = true) {
         delay(2000)
-        // Después de 5 seg navegamos a FirstScreen
         navController.popBackStack()
         navController.navigate("travel")
     }
-
     MyTravelSplashScreen()
 }
 
@@ -47,6 +41,7 @@ fun MyTravelSplashScreen() {
         .background(Color(0xff4B92FF)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(modifier = Modifier.height(64.dp))
         Image(painter = painterResource(id = R.drawable.logo), contentDescription = "logo",
             modifier = Modifier.size(150.dp, 150.dp))
         Text(text = "MyTravel",
@@ -54,14 +49,17 @@ fun MyTravelSplashScreen() {
             fontSize = 30.sp,
             fontWeight = FontWeight.Bold,
             fontFamily = MyTypography.fontFamily)
-        Spacer(modifier = Modifier.height(64.dp))
-        Text(text = "by Iván García Gómez",
+        Spacer(modifier = Modifier.height(128.dp))
+        Text(text = "Desarrollado por",
+            color = Color.White,
+            fontSize = 18.sp,
+            fontFamily = MyTypography.fontFamily)
+        Text(text = "Iván García Gómez",
             color = Color.White,
             fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
             fontFamily = MyTypography.fontFamily)
-        Spacer(modifier = Modifier.height(64.dp))
     }
-
 }
 
 @Preview
--- app/src/main/java/com/ivione93/mytravel2/screens/TravelScreen.kt
+++ app/src/main/java/com/ivione93/mytravel2/screens/TravelScreen.kt
@@ -1,11 +1,13 @@
 package com.ivione93.mytravel2.screens
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,7 +17,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.TravelExplore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -26,18 +27,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.ivione93.mytravel2.AppNavigation
 import com.ivione93.mytravel2.R
 import com.ivione93.mytravel2.models.City
 import com.ivione93.mytravel2.models.TravelModel
 import com.ivione93.mytravel2.ui.theme.MyTypography
-
 
 @Composable
 fun TravelScreen(modifier: Modifier, travelModel: TravelModel, onCityClick: (City) -> Unit) {
@@ -63,23 +63,44 @@ fun TravelScreen(modifier: Modifier, travelModel: TravelModel, onCityClick: (Cit
 
 @Composable
 fun MyHeader(modifier: Modifier) {
-    Row(
+    Box(
         modifier = modifier
-            .fillMaxWidth()
             .height(200.dp)
-            .background(
-                brush = Brush.verticalGradient(
-                    colors = listOf(Color(0xff4B92FF), Color(0xff4B92FF).copy(0.02f))
-                )
-            )
-            .padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .fillMaxWidth()
     ) {
-        Icon(painter = painterResource(id = R.drawable.logo), contentDescription = "Logo",
-            Modifier
-                .size(100.dp)
-                .padding(horizontal = 8.dp), tint = Color.White)
-        Text(text = "MyTravel", color = Color.White, fontSize = 40.sp, fontWeight = FontWeight.Bold, fontFamily = MyTypography.fontFamily)
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            val width = size.width
+            val height = size.height
+
+            val path = Path().apply {
+                lineTo(0f, height * 0.85f)
+                quadraticBezierTo(width * 0.25f, height * 0.95f, width * 0.5f, height * 0.85f)
+                quadraticBezierTo(width * 0.75f, height * 0.75f, width, height * 0.90f)
+                lineTo(width, 0f)
+                close()
+            }
+
+            drawPath(
+                path = path,
+                color = Color(0xff4B92FF),
+                style = Fill
+            )
+        }
+        Row(
+            modifier = Modifier
+                .height(200.dp)
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(painter = painterResource(id = R.drawable.logo), contentDescription = "Logo",
+                Modifier
+                    .size(100.dp)
+                    .padding(horizontal = 8.dp), tint = Color.White)
+            Text(text = "MyTravel", color = Color.White, fontSize = 40.sp, fontWeight = FontWeight.Bold, fontFamily = MyTypography.fontFamily)
+        }
     }
 }
 
